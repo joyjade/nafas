d3a72dfaced34671e96ce176c7263dead0f58cb1
JJ <jadeandpearls@gmail.com>
2025-08-29 13:44:14 -0600

    events;

--- site/templates/events.php
+++ site/templates/events.php
@@ -1,7 +1,20 @@
 <?php snippet('nav') ?>
-<div class="">
-  <?php foreach($page->children()->listed() as $event) : ?>
-    <h1><?= $event->title() ?></h1>
-  <?php endforeach ?>
-</div>
+<?php foreach($page->children()->listed() as $event) : ?>
+  <div class="event">
+    <a href="<?= $event-> url() ?>">
+    <h1>
+      <?= $event->title() ?>
+    </h1>
+    <date><?= $event->start()->toDate('l, jS \of F, g:i') ?>-<?= $event->end()->toDate('g:i A') ?></date>
+    <p>
+      <?= $event->description() ?>
+    </p>
+    <!-- <?php if ($photo = $event->photo()->toFile()) : ?>
+    <figure>
+      <img src="<?= $photo->url() ?>" alt="<?= $photo->alt() ?>">
+    </figure>
+    <?php endif ?> -->
+  </a>
+  </div>
+<?php endforeach ?>
 <?php snippet('footer') ?>
