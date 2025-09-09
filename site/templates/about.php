f6fac5b7b52cf18d82502ba3c81099ace5955264
JJ <jadeandpearls@gmail.com>
2025-09-08 23:22:46 -0600

    lungs added;

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,6 +1,22 @@
 <?php snippet('nav') ?>
 <main class="">
   <section class="intro">
+    <div class="lung-container">
+      <div class="lung">
+        <span class="red">(</span>
+        <span class="blue">(</span>
+        <span class="sage">(</span>
+        <span class="red">(</span>
+        <span class="sage">(</span>
+    </div>
+    <div class="lung">
+        <span class="red">(</span>
+        <span class="blue">(</span>
+        <span class="sage">(</span>
+        <span class="red">(</span>
+        <span class="sage">(</span>
+    </div>
+    </div>
     <?= $page->intro()->toBlocks() ?>
     <?= $page->text()->kt() ?>
   </section>
